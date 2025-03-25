8eb99d09848d067d258d2f79e315d2ebf6df3f77
Mohamed Saad <mosaad05019@gmail.com>
2025-03-25 17:11:25 +0200

    front-pages

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,39 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+//
+//require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+ * Front Site Routes
+ */
+Route::prefix('/')->name('front.')->group(function () {
+    # ======================================= index page
+    Route::view('', 'front.index')->name('index');
+    # ======================================= about page
+    Route::view('about', 'front.about')->name('about');
+    # ======================================= contact page
+    Route::view('contact', 'front.contact')->name('contact');
+    # ======================================= projects page
+    Route::view('projects', 'front.projects')->name('projects');
+    # ======================================= services page
+    Route::view('services', 'front.services')->name('services');
+    # ======================================= team page
+    Route::view('team', 'front.team')->name('team');
+    # ======================================= testimonial page
+    Route::view('testimonial', 'front.testimonial')->name('testimonial');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
